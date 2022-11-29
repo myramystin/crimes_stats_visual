6a63f9b57104206514e622949be2d2a1c864cf72
Dasha <onikovad@gmail.com>
2022-11-29 16:06:43 +0300

    task done

--- main.py
+++ main.py
@@ -1,62 +1,112 @@
 from dash import Dash, html, dcc, Input, Output
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
 
-from prepare_data import df_income, df_population, df_pct_unemp, df_pct_urban
-
-"""
-selectors -- 
-    * state name - dropdown
-    * population - slider 
-    * what crimes to include - checkbox
-    * medIncome - slider
-    * pct urban - slider
-    * pct unemployed - slider
-"""
-
-
+from prepare_data import df
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = Dash(__name__, external_stylesheets=external_stylesheets)
 
-app.layout = html.Div([
-    dcc.Dropdown(["Population", "Median Income", "Urban Percentage",
-                  "Unemployed Percentage"], "Population", id="feature"),
-    dcc.Graph(id='graphic'),
+app.layout = html.Div(children=[
+
+    html.Div([
+        html.Div([
+            dcc.Dropdown(options={"population": "Population", "medIncome": "Median Income",
+                                  "pctDiv": "Percentage Divorced",
+                                  "pctNotSpeakEnglWell": "Percentage Not Speaking English Well",
+                                  "pctUnemployed": "Percentage Unemployed",
+                                  "pctUrban": "Percentage Urban"}, value="population", id="feature")],
+            style={'width': '49%', 'display': 'inline-block'}),
+        html.Div([
+            dcc.Dropdown([i for i in df["state"].unique()], "AK", id="state")], style={'width': '49%',
+                                                                                       'float': 'right',
+                                                                                       'display': 'inline-block'}),
+        dcc.Graph(id='graphic')
+    ]),
+    html.Div([
+        dcc.Graph(id='full-graphic'),
+        html.Div("Choose maximum median Income"),
+        dcc.Slider(id="medincome_range",
+                   min=df["medIncome"].min(),
+                   max=df["medIncome"].max(),
+                   step=10000),
+
+        html.Div("Choose maximum Urban Percentage"),
+        dcc.Slider(id="pcturban_range",
+                   min=df["pctUrban"].min(),
+                   max=df["pctUrban"].max(),
+                   step=10
+                   ),
+
+        html.Div("Choose maximum Unemployed Percentage"),
+        dcc.Slider(id="pctunemp_range",
+                   min=df["pctUnemployed"].min(),
+                   max=df["pctUnemployed"].max(),
+                   step=10
+                   ),
+        html.Div("Choose maximum Not Speak English Well Percentage"),
+        dcc.Slider(id="pctnoeng_range",
+                   min=df["pctNotSpeakEnglWell"].min(),
+                   max=df["pctNotSpeakEnglWell"].max(),
+                   step=10
+                   ),
+
+        html.Div("Choose maximum divorce Percentage"),
+        dcc.Slider(id="pctdiv_range",
+                   min=df["pctDiv"].min(),
+                   max=df["pctDiv"].max(),
+                   step=10
+                   ),
+    ])
 ])
 
 
 @app.callback(
     Output('graphic', 'figure'),
-    Input('feature', 'value'))
-def update_graph(feature_value):
-    fig = None
-    if feature_value == "Population":
-        fig = px.scatter(
-            x=df_population["population"],
-            y=df_population["arsons"])
-    elif feature_value == "Median Income":
-        fig = px.scatter(
-            x=df_income["rounded_income"],
-            y=df_income["arsons"])
-    elif feature_value == "Urban Percentage":
-        fig = px.scatter(
-            x=df_pct_urban["rounded_pct"],
-            y=df_pct_urban["arsons"])
-    else:
-        fig = px.scatter(
-            x=df_pct_urban["rounded_pct"],
-            y=df_pct_urban["arsons"])
+    Input('feature', 'value'),
+    Input('state', 'value'))
+def update_graph(feature_value, state_value):
+    df_filtered = df[df["state"] == state_value]
 
-    fig.update_layout(margin={'l': 40, 'b': 40, 't': 10, 'r': 0},
-                      hovermode='closest')
-
+    fig = px.histogram(
+        df_filtered,
+        y=["murders", "rapes", "robberies", "assaults", "burglaries", "larcenies", "autoTheft", "arsons"],
+        x=feature_value,
+        nbins=50)
+    fig.update_layout(margin={'l': 40, 'b': 40, 't': 30, 'r': 40},
+                      hovermode='closest',
+                      xaxis_title=feature_value,
+                      yaxis_title="Crimes Count",
+                      title="Crimes Count by Different Features")
 
     return fig
 
 
+@app.callback(
+    Output('full-graphic', 'figure'),
+    Input('medincome_range', 'value'),
+    Input('pcturban_range', 'value'),
+    Input('pctunemp_range', 'value'),
+    Input('pctnoeng_range', 'value'),
+    Input('pctdiv_range', 'value'))
+def update_full_graph(med_income, pct_urban, pct_unemp, pct_noeng, pct_div):
+    local_df = df[df["medIncome"] <= med_income]
+    local_df = local_df[local_df["pctUrban"] <= pct_urban]
+    local_df = local_df[local_df["pctUnemployed"] <= pct_unemp]
+    local_df = local_df[local_df["pctNotSpeakEnglWell"] <= pct_noeng]
+    local_df = local_df[local_df["pctDiv"] <= pct_div]
 
+    fig = px.histogram(local_df,
+                       x="population",
+                       y=["murders", "rapes", "robberies", "assaults", "burglaries", "larcenies", "autoTheft",
+                          "arsons"])
+
+    fig.update_layout(margin={'l': 40, 'b': 40, 't': 30, 'r': 40}, hovermode='closest',
+                      xaxis_title="Population",
+                      yaxis_title="Crimes Count")
+    return fig
 
 
 if __name__ == '__main__':
--- prepare_data.py
+++ prepare_data.py
@@ -1,68 +1,30 @@
 import pandas as pd
-import numpy as np
 
-
-df = pd.read_csv("crimedata.csv")[["state", "communityName", "population", "medIncome", "numbUrban", "PctUnemployed", "murders", "rapes", "robberies", "assaults", "burglaries", "larcenies", "autoTheft", "arsons"]]
+df = pd.read_csv("crimedata.csv")[["state", "communityName", "population", "medIncome", "numbUrban", "PctUnemployed",
+                                   "PctNotSpeakEnglWell", "TotalPctDiv",
+                                   "murders", "rapes", "robberies", "assaults", "burglaries", "larcenies", "autoTheft",
+                                   "arsons"]]
 df = df.dropna()
-df["numbUnemployed"] = df["population"]*df["PctUnemployed"]/100
-df = df.drop("PctUnemployed", axis=1)
-df = df.groupby(["state", "communityName"]).agg({"population":"sum", "numbUrban":"mean", "numbUnemployed":"mean", "medIncome":"mean", "murders":"sum", "rapes":"sum", "robberies":"sum", "assaults":"sum", "burglaries":"sum",
-                              "larcenies":"sum", "autoTheft":"sum", "arsons":"sum"})
-df["pctUnemployed"] = 100*df["numbUnemployed"]/df["population"]
-df["pctUrban"] = 100*df["numbUrban"]/df["population"]
-df = df.drop("numbUnemployed", axis=1)
-df = df.drop("numbUrban", axis=1)
+df["numbUnemployed"] = df["population"] * df["PctUnemployed"] / 100
+df["numbNotSpeakEnglWell"] = df["population"] * df["PctNotSpeakEnglWell"] / 100
+df["numbDiv"] = df["population"] * df["TotalPctDiv"] / 100
+df = df.drop(["PctUnemployed", "PctNotSpeakEnglWell", "TotalPctDiv"], axis=1)
+df = df.groupby(["state", "communityName"]).agg({"population": "sum", "numbUrban": "sum",
+                                                 "numbUnemployed": "sum",
+                                                 "numbNotSpeakEnglWell": "sum", "numbDiv": "sum",
+                                                 "medIncome": "mean",
+                                                 "murders": "sum", "rapes": "sum",
+                                                 "robberies": "sum", "assaults": "sum", "burglaries": "sum",
+                                                 "larcenies": "sum", "autoTheft": "sum", "arsons": "sum"})
+df["pctDiv"] = (100 * df["numbDiv"] / df["population"]).apply(lambda x: int(x))
+df["pctNotSpeakEnglWell"] = (100 * df["numbNotSpeakEnglWell"] / df["population"]).apply(lambda x: int(x))
+df["pctUnemployed"] = (100 * df["numbUnemployed"] / df["population"]).apply(lambda x: int(x))
+df["pctUrban"] = (100 * df["numbUrban"] / df["population"]).apply(lambda x: int(x))
+df["medIncome"] = df["medIncome"].apply(lambda x: round(x))
+df = df.drop(["numbUnemployed", "numbUrban", "numbNotSpeakEnglWell"], axis=1)
+df["crimes_count"] = df["murders"] + df["rapes"] + df["robberies"] + df["assaults"] + df["burglaries"] + df[
+    "larcenies"] + df["autoTheft"] + df["arsons"]
+
 df = df.reset_index()
 
-
-
-df_population = df.groupby(["state", "population"]).agg({"murders":"mean", "rapes":"mean", "robberies":"mean", "assaults":"mean", "burglaries":"mean",
-                                                         "larcenies":"mean", "autoTheft":"mean", "arsons":"mean"}).reset_index()
-
-
-
-
-df_income = df.groupby(["state", "medIncome"]).agg({"murders":"mean", "rapes":"mean", "robberies":"mean", "assaults":"mean", "burglaries":"mean",
-                                                     "larcenies":"mean", "autoTheft":"mean", "arsons":"mean"}).reset_index()
-df_income["rounded_income"] = df_income["medIncome"]/100000
-df_income["rounded_income"] = df_income["rounded_income"].apply(lambda x: round(x, 2))*100000
-
-df_income = df_income.groupby(["state", "rounded_income"]).agg(
-    {"murders": "mean", "rapes": "mean", "robberies": "mean", "assaults": "mean", "burglaries": "mean",
-     "larcenies": "mean", "autoTheft": "mean", "arsons":"mean"}).reset_index()
-
-
-
-
-
-df_pct_unemp = df.groupby(["state", "pctUnemployed"]).agg({"murders":"mean", "rapes":"mean", "robberies":"mean", "assaults":"mean", "burglaries":"mean",
-                                                      "larcenies":"mean", "autoTheft":"mean", "arsons":"mean"}).reset_index()
-
-df_pct_unemp["rounded_pct"] = df_pct_unemp["pctUnemployed"]/100
-df_pct_unemp["rounded_pct"] = df_pct_unemp["rounded_pct"].apply(lambda x: round(x, 1))*100
-
-df_pct_unemp = df_pct_unemp.groupby(["state", "rounded_pct"]).agg(
-    {"murders": "mean", "rapes": "mean", "robberies": "mean", "assaults": "mean", "burglaries": "mean",
-     "larcenies": "mean", "autoTheft": "mean", "arsons":"mean"}).reset_index()
-
-
-
-
-df_pct_urban = df.groupby(["state", "pctUrban"]).agg({"murders":"mean", "rapes":"mean", "robberies":"mean", "assaults":"mean", "burglaries":"mean",
-                                                           "larcenies":"mean", "autoTheft":"mean"}).reset_index()
-
-df_pct_urban["rounded_pct"] = df_pct_urban["pctUrban"]/100
-df_pct_urban["rounded_pct"] = df_pct_urban["rounded_pct"].apply(lambda x: round(x, 1))*100
-
-df_pct_urban = df_pct_urban.groupby(["state", "rounded_pct"]).agg(
-    {"murders": "mean", "rapes": "mean", "robberies": "mean", "assaults": "mean", "burglaries": "mean",
-     "larcenies": "mean", "autoTheft": "mean"}).reset_index()
-
-
-
-
-
-
-
-
-
+# print(df.head())
